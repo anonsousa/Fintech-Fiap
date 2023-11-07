314c1ff50610c135e8ad0a3fa63e2b0353b94446
Antonio Sousa <pisouza011@gmail.com>
2023-11-07 13:16:55 -0300

    Tentando arrumar a validacao de login e password pegando os dados do oracle

---
+++ Fintech/src/fintech/dao/FintechDAO.java
@@ -0,0 +1,100 @@
+package fintech.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class FintechDAO {
+    private static final String URL = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL"; 
+    private static final String USERNAME = "rm551647";
+    private static final String PASSWORD = "260698";
+
+    private Connection connection;
+
+    public FintechDAO() {
+        try {
+            // Carrega o JDBC
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Driver JDBC do Oracle não encontrado.");
+        }
+    }
+
+    public void conectar() {
+        try {
+            // Estabelecer Conexao com a DB
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            if (connection != null) {
+                System.out.println("Conexão bem-sucedida com o banco de dados Oracle.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Erro ao conectar ao banco de dados Oracle: " + e.getMessage());
+        }
+    }
+
+    public void desconectar() {
+        try {
+            if (connection != null) {
+                connection.close();
+                System.out.println("Conexão com o banco de dados Oracle encerrada.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Erro ao encerrar a conexão com o banco de dados Oracle: " + e.getMessage());
+        }
+    }
+    
+    
+    
+    
+    //Validar Login
+    public boolean credenciaisValidas(String login, String senha) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        try {
+            // Preparar a consulta SQL para buscar o usuário com o login e senha especificados
+            String sql = "SELECT * FROM T_USUARIO WHERE NM_EMAIL = ? AND PW_USUARIO = ?";
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, login);
+            preparedStatement.setString(2, senha);
+
+            resultSet = preparedStatement.executeQuery();
+
+            // Se houver uma linha correspondente, as credenciais são válidas
+            return resultSet.next();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Erro ao validar as credenciais: " + e.getMessage());
+        } finally {
+            // Fechar recursos (ResultSet, PreparedStatement e Connection)
+            if (resultSet != null) {
+                try {
+                    resultSet.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (preparedStatement != null) {
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
--- Fintech/src/fintech/model/Login.java
+++ Fintech/src/fintech/model/Login.java
@@ -1,5 +1,8 @@
 package fintech.model;
 
+import fintech.dao.FintechDAO;
+
+
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,9 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/Login")
 public class Login extends HttpServlet {
-    /**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -19,26 +20,23 @@ public class Login extends HttpServlet {
         // Lógica do tratamento do login por GET
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        String login = request.getParameter("login");
-        String senha = request.getParameter("senha");
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+		    throws ServletException, IOException {
+		    String login = request.getParameter("login");
+		    String senha = request.getParameter("senha");
+		    
+		    FintechDAO fintechDAO = new FintechDAO();
+		    fintechDAO.conectar();
 
-        if (credenciaisValidas(login, senha)) {
-            // Redirecionar para a página principal
-            response.sendRedirect("index.jsp");
-        } else {
-            // Exibir uma mensagem de erro
-            request.setAttribute("erro", "Credenciais inválidas. Por favor, tente novamente.");
-            request.getRequestDispatcher("home.jsp").forward(request, response);
-        }
-    }
+		    if (fintechDAO.credenciaisValidas(login, senha)) {
+		        // Redirecionar para a página principal
+		        response.sendRedirect("index.jsp");
+		    } else {
+		        // Exibir uma mensagem de erro
+		        request.setAttribute("erro", "Credenciais inválidas. Por favor, tente novamente.");
+		        request.getRequestDispatcher("home.jsp").forward(request, response);
+		    }
 
-    private boolean credenciaisValidas(String login, String senha) {
-        // Implemente a lógica real para verificar as credenciais do usuário aqui.
-        // Isso pode envolver a consulta de um banco de dados ou outra fonte de dados.
-
-        // Exemplo de verificação simples para fins de teste:
-        return "test@email.com".equals(login) && "123".equals(senha);
-    }
+		    fintechDAO.desconectar(); // Desconecta do banco de dados
+		}
 }
